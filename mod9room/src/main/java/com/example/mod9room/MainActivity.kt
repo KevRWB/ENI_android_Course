cbf7352e66e5f0ff0a07d02845e9a7107c7becff
Kevin RENAULT <99198628+KevRWB@users.noreply.github.com>
2022-11-30 17:01:12 +0100

    factory

--- mod9room/src/main/java/com/example/mod9room/MainActivity.kt
+++ mod9room/src/main/java/com/example/mod9room/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.mod9room
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
+
+    val viewModel: OiseauViewModel by viewModels {OiseauViewModel.Factory}
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
     }
 }
