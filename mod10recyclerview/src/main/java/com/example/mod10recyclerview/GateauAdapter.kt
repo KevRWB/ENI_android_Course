dabbe7cea34a1e809386aba9b1d4e0769bc93b53
Kevin RENAULT <99198628+KevRWB@users.noreply.github.com>
2022-12-01 14:33:30 +0100

    recyclerView gato

--- mod10recyclerview/src/main/java/com/example/mod10recyclerview/GateauAdapter.kt
+++ mod10recyclerview/src/main/java/com/example/mod10recyclerview/GateauAdapter.kt
@@ -1,6 +1,33 @@
 package com.example.mod10recyclerview
 
-class GateauAdapter {
-    
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.example.mod10recyclerview.databinding.TemplateLineGateauBinding
+
+class GateauAdapter(val alGateaux : ArrayList<Gateau>) : RecyclerView.Adapter<GateauAdapter.GateauVH>() {
+
+
+    class GateauVH(val bindingLineGateau : TemplateLineGateauBinding)
+        : ViewHolder(bindingLineGateau.root){
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GateauVH {
+
+        val binding = TemplateLineGateauBinding.inflate(
+            LayoutInflater.from(parent.context),parent, false)
+
+        return GateauVH(binding)
+    }
+
+    override fun onBindViewHolder(holder: GateauVH, position: Int) {
+        val gateauToDisplay = alGateaux[position]
+        holder.bindingLineGateau.gateau=gateauToDisplay
+    }
+
+    override fun getItemCount(): Int = alGateaux.size
+
 
 }
--- mod10recyclerview/src/main/java/com/example/mod10recyclerview/MainActivity.kt
+++ mod10recyclerview/src/main/java/com/example/mod10recyclerview/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.mod10recyclerview
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val alGateaux = arrayListOf(
+            Gateau("Gateau chocolat", false, 4, "chocolaté" ),
+            Gateau("Gateau citron", false, 3, "citronné" ),
+            Gateau("Gateau vanille", true, 4, "vanillé" ),
+            Gateau("Gateau praline", false, 10, "praliné" )
+        )
+
+        val adapter = GateauAdapter(alGateaux)
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerViewGateau)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = adapter
+
+
     }
 }
